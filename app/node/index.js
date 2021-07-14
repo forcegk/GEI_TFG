0deaa173d672a2677923d8a444103542438fed8a
Alonso Rodriguez <alonso.rodriguez@udc.es>
2021-07-14 23:55:09 +0200

    Websockets partially done

--- app/node/index.js
+++ app/node/index.js
@@ -5,20 +5,9 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const { spawn } = require('child_process');
-
-// GET Zone
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-
-// app.get('/monitoring.html', (req, res) => {
-//     res.sendFile(__dirname + '/monitoring.html');
-// });
-
-// app.get('/style.css' , (req , res)=>{
-//     res.sendFile(__dirname + '/style.css');
-// })
+const { spawn, exec } = require('child_process');
+const utf8 = require('utf8');
+const { exit } = require('process');
 
 // Add webpage dir
 app.use(express.static('www'));
@@ -28,28 +17,39 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        // pkill benchmark
+        // Solo funciona en dispositivos *NIX
+        exec('killall mpirun', () => {});
     });
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        // io.emit general
-        socket.emit('chat message', msg);
-    });
+    socket.on('spawn', (bench_name) => {
+        console.log('User ' + socket.id + ' called benchmark: ' + bench_name);
 
-});
-
-io.on('connection', (socket) => {
-    socket.on('spawn_1', () => {
+        // TODO CAMBIAR EN LINUX
+        // OJO IMPORTANTE HACER UN SWITCH CON BENCH NAME Y NO METERLO DIRECTO AL COMANDO
+        var command;
+        switch (bench_name) {
+            case 'lu':
+            case 'cg':
+            case 'ft':
+            case 'is':
+            case 'mg':
+            case 'ep':
+                command = bench_name;
+                break;
+        
+            default:
+                command = 'neofetch';
+                break;
+        }
         const ping = spawn('ping', ['www.google.es']);
         ping.stdout.on('data', (data) => {
-            socket.emit('spawn_1', `${data}`);
+            socket.emit('youve_got_mail', utf8.encode(data.toString()));
+            //  io.emit // Para enviarlo a todos los usuarios conectados
         });
-
-        //socket.emit('spawn_1', "test_spawn_1");
     });
+
 });
 
 server.listen(3000, () =>{
